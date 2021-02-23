f200c8496c84a4cd8ae37b96bf5728243a9813a0
lil_gappy_69 <49754258+gaponukz@users.noreply.github.com>
2021-02-23 19:08:42 +0200

    Update help_bot.py

--- help_bot.py
+++ help_bot.py
@@ -1,7 +1,20 @@
-from aiogram import (
-    Bot, Dispatcher, executor, types
-)
+from aiogram.utils.executor import start_webhook
+from aiogram.dispatcher import Dispatcher
+from aiogram import Bot, types
+
 import asyncio, os
+import aioschedule
+import json
+import logging
+
+TOKEN = "1549669090:AAEtDBdzYf_le6Dvx_DRNiO2qtBOIeG9lAM"
+
+WEBHOOK_HOST = 'https://zhenyagaponukhelpbot.herokuapp.com'
+WEBHOOK_PATH = '/webhook/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ.get('PORT')
 
 bot = Bot(token = '1632180424:AAHiioPQ0uCxSi-p4sQ5MoHtiEc0Gsiw3Dk')
 bot_name = 'Zhenyagaponuk Bot'
@@ -190,5 +203,19 @@ async def start_bot(message: types.Message):
         reply_markup = hello_buttons
     )
 
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # asyncio.create_task(sheduler())
+
+async def on_shutdown(dp):
+    pass
+
 if __name__ == '__main__':
-	executor.start_polling(dp, skip_updates = True)
+    start_webhook(
+        dispatcher = dp,
+        webhook_path = WEBHOOK_PATH,
+        on_startup = on_startup,
+        on_shutdown = on_shutdown,
+        host = WEBAPP_HOST,
+        port = WEBAPP_PORT
+    )
